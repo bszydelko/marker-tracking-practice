2b0dc89c61b005aae61ca17ceb0ae5fc5c1101f6
Błażej Szydełko <blazej.szydelko@student.put.poznan.pl>
2020-07-28 14:15:18 +0200

    utils

--- MultiCameraLightTracking/LightTracker.cpp
+++ MultiCameraLightTracking/LightTracker.cpp
@@ -5,6 +5,9 @@ namespace bs
 		: m_video(video)
 	{
 		//initialize windows
+		cv::namedWindow(m_sPreviousFrame, cv::WINDOW_KEEPRATIO);
+		cv::resizeWindow(m_sPreviousFrame, m_video->getResolution() / 2);
+
 		cv::namedWindow(m_sCurrentFrame, cv::WINDOW_KEEPRATIO);
 		cv::resizeWindow(m_sCurrentFrame, m_video->getResolution() / 2);
 
@@ -19,6 +22,14 @@ namespace bs
 		cv::namedWindow(m_sBulb, cv::WINDOW_KEEPRATIO);
 		cv::resizeWindow(m_sBulb, m_video->getResolution() / 2);
 
+		cv::namedWindow(m_sContoursMoments, cv::WINDOW_KEEPRATIO);
+		cv::resizeWindow(m_sContoursMoments, m_video->getResolution());
+
+		m_kernelDilate_thresholdLights = cv::getStructuringElement(cv::MORPH_DILATE, cv::Size(9, 9));
+		m_kernelDilate_detectBulb = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(11, 11));
+
+
+
 	}
 
 	int32_t LightTracker::start()
@@ -26,36 +37,44 @@ namespace bs
 		//thresh1
 		m_video->read(m_imgCurrentFrame);
 		m_frameCounter++;
-
 		thresholdLights(m_imgCurrentFrame, m_imgLightThresh1);
-		cv::imshow(m_sLightThresh1, m_imgLightThresh1);
+
 		//thresh2
 		m_video->read(m_imgCurrentFrame);
 		m_frameCounter++;
-
 		thresholdLights(m_imgCurrentFrame, m_imgLightThresh2);
-		cv::imshow(m_sLightThresh2, m_imgLightThresh2);
 
 		createLightMask(m_imgLightThresh1, m_imgLightThresh2, m_imgLightMask);
-		cv::imshow(m_sLightMask, m_imgLightMask);
 
-		while (true)
+		m_video->read(m_imgFirstBulbFrame);
+		m_frameCounter++;
+		cv::Point2d bulbFirstPos = detectBulbInFirstFrame(m_imgFirstBulbFrame, m_imgLightMask);
+		m_vecBulbs.emplace_back(bs::Bulb(bulbFirstPos, m_frameCounter));
+
+		cv::circle(m_imgFirstBulbFrame, bulbFirstPos, 10, cv::Scalar(234, 247, 29), 3);
+		cv::imshow("first bulb pos", m_imgFirstBulbFrame);
+		m_imgFirstBulbFrame.copyTo(m_imgPreviousFrame);
+
+		while (m_video->read(m_imgCurrentFrame))
 		{
+			m_frameCounter++;
 
 			if (cv::waitKey(5) == 27) break;
-			if (m_video->read(m_imgCurrentFrame))
-			{
-				cv::waitKey(0);
-				m_bulbPos = detectLight(m_imgCurrentFrame, m_imgLightMask);
-				cv::circle(m_imgCurrentFrame, m_bulbPos, 10, cv::Scalar(0, 255, 0), 3);
-				cv::imshow(m_sCurrentFrame, m_imgCurrentFrame);
+			
+			cv::waitKey(0);
 
-				std::cout << m_frameCounter << m_bulbPos << std::endl;
+			m_bulbPos = detectBulb(m_imgCurrentFrame, m_imgLightMask);
+			cv::circle(m_imgCurrentFrame, m_bulbPos, 10, cv::Scalar(0, 255, 0), 3);
 
-				m_frameCounter++;
-			}
-			else return bs::END_OF_FILE; //TODO - handle state in main
+			//imshow(true, true, true, true, true, true, true);
+			imshow(false, true, false, false, false, true, false);
+			std::cout << m_frameCounter << m_bulbPos << std::endl;
+
+			m_imgCurrentFrame.copyTo(m_imgPreviousFrame);
+
 		}
+
+		return 1;
 	}
 
 	LightTracker::~LightTracker()
@@ -65,10 +84,8 @@ namespace bs
 
 	void LightTracker::thresholdLights(const cv::Mat& frame, cv::Mat& imgThresh)
 	{
-		m_kernelDilate_thresholdLights = cv::getStructuringElement(cv::MORPH_DILATE, cv::Size(9, 9));
 		//cv::Mat m_kernelErode_detectLight = cv::getStructuringElement()
 
-		m_thresh_thresholdLights = 210;
 		cv::cvtColor(frame, imgThresh, cv::COLOR_BGR2GRAY);
 		cv::blur(imgThresh, imgThresh, cv::Size(9, 9));
 		cv::dilate(imgThresh, imgThresh, m_kernelDilate_thresholdLights);
@@ -80,78 +97,206 @@ namespace bs
 		cv::bitwise_and(frame1, frame2, mask);
 	}
 
-	cv::Point2f LightTracker::detectLight(const cv::Mat& frame, const cv::Mat& mask)
+	cv::Point2d LightTracker::detectBulb(const cv::Mat& frame, const cv::Mat& mask)
 	{
-		m_kernelDilate_detectLight = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(11, 11));
 
-		thresholdLights(frame, m_imgThresh_detectLight);
-		cv::absdiff(m_imgThresh_detectLight, mask, m_imgBulb_detectLight);
-		cv::GaussianBlur(m_imgBulb_detectLight, m_imgBulb_detectLight, cv::Size(3, 3), 0);
-		cv::threshold(m_imgBulb_detectLight, m_imgBulb_detectLight, 220, 255, cv::THRESH_BINARY);
-		cv::dilate(m_imgBulb_detectLight, m_imgBulb_detectLight, m_kernelDilate_detectLight);
+		thresholdLights	(frame, m_imgThresh_detectBulb);
+		cv::absdiff		(m_imgThresh_detectBulb, mask, m_imgBulb_detectBulb);
+		cv::GaussianBlur(m_imgBulb_detectBulb, m_imgBulb_detectBulb, cv::Size(3, 3), 0);
+		cv::threshold	(m_imgBulb_detectBulb, m_imgBulb_detectBulb, 220, 255, cv::THRESH_BINARY);
+		cv::dilate		(m_imgBulb_detectBulb, m_imgBulb_detectBulb, m_kernelDilate_detectBulb);
 
-		cv::imshow(m_sBulb, m_imgBulb_detectLight);
-	
 		//check if bulb overlaps with another light source
 		//check if another object cover mask
-
-
-
 
 		//contours
 	
 		cv::Canny(
-			m_imgBulb_detectLight, 
-			m_imgCanny_detectLight, 50, 150, 3);
+			m_imgBulb_detectBulb, 
+			m_imgCanny_detectBulb, 50, 150, 3);
 		cv::findContours(
-			m_imgCanny_detectLight, 
-			m_vecContours_detectLight, 
-			m_vecHierarchy_detectLight, 
-			cv::RETR_TREE, 
+			m_imgCanny_detectBulb, 
+			m_vecContours_detectBulb, 
+			m_vecHierarchy_detectBulb, 
+			cv::RETR_LIST, 
 			cv::CHAIN_APPROX_SIMPLE, 
 			cv::Point(0, 0));
 
-		m_vecMoments_detectLight.clear();
-		m_vecMoments_detectLight.reserve(m_vecContours_detectLight.size());
-		m_vecCentralMoments_detectLight.clear();
-		m_vecCentralMoments_detectLight.reserve(m_vecContours_detectLight.size());
+		m_vecMoments_detectBulb.clear();
+		m_vecMoments_detectBulb.reserve(m_vecContours_detectBulb.size());
+		m_vecCentralMoments_detectBulb.clear();
+		m_vecCentralMoments_detectBulb.reserve(m_vecContours_detectBulb.size());
 		
 
-		for (int i = 0; i < m_vecContours_detectLight.size(); i++)
+		for (int i = 0; i < m_vecContours_detectBulb.size(); i++)
 		{
-			m_vecMoments_detectLight[i] = 
-				cv::moments(m_vecContours_detectLight[i], false); 		//moments for each blob / contour
-			m_vecCentralMoments_detectLight[i] = 
+			m_vecMoments_detectBulb[i] = 
+				cv::moments(m_vecContours_detectBulb[i], false); 		//moments for each blob / contour
+			m_vecCentralMoments_detectBulb[i] = 
+				cv::Point2d(											//center of each blob / contour
+					m_vecMoments_detectBulb[i].m10 / m_vecMoments_detectBulb[i].m00, 
+					m_vecMoments_detectBulb[i].m01 / m_vecMoments_detectBulb[i].m00); 		
+		}
+
+		bs::Bulb prevBulb = m_vecBulbs.back();
+		cv::Point2d retPoint;
+
+
+		if (m_vecContours_detectBulb.size() == 1)
+		{
+			bool tmp = bulbVsMask(m_vecContours_detectBulb[0], mask);  
+			retPoint = m_vecCentralMoments_detectBulb[0];
+			m_vecBulbs.emplace_back(bs::Bulb(retPoint, m_frameCounter));
+			m_vecBulbs.back().setMotion(&prevBulb);
+
+		}
+		else if (m_vecContours_detectBulb.size() > 1) //chose biggest
+		{
+			double area1 = 0.0;
+			double area2 = 0.0;
+
+			if (m_vecContours_detectBulb.size() > 1) {
+				area1 = cv::contourArea(m_vecContours_detectBulb[0]);
+				retPoint = m_vecCentralMoments_detectBulb[0];
+
+				for (int i = 1; i < m_vecContours_detectBulb.size(); i++) {
+					area2 = cv::contourArea(m_vecContours_detectBulb[i]);
+					if (area2 > area1) {
+						retPoint = m_vecCentralMoments_detectBulb[i];
+						area1 = area2;
+					}
+				}
+			}
+
+			m_vecBulbs.emplace_back(bs::Bulb(retPoint, m_frameCounter));
+			m_vecBulbs.back().setMotion(&prevBulb);
+
+		}
+		else 
+		{
+			retPoint = cv::Point(0, 0);
+			m_vecBulbs.emplace_back(bs::Bulb(retPoint, m_frameCounter, false));
+		}
+
+		return retPoint;
+	}
+
+	cv::Point2d LightTracker::detectBulbInFirstFrame(const cv::Mat& frame, const cv::Mat& mask)
+	{
+		cv::Mat frame_thresh;
+		cv::Mat frame_bulb;
+		cv::Mat								frame_canny;
+		std::vector<std::vector<cv::Point>> vecContours;
+		std::vector<cv::Vec4i>				vecHierarchy;
+		std::vector<cv::Moments>			vecMoments;
+		std::vector<cv::Point2f>			vecCentralMoments;
+
+		thresholdLights(frame, frame_thresh);
+		cv::absdiff(frame_thresh, mask, frame_bulb);
+		cv::GaussianBlur(frame_bulb, frame_bulb, cv::Size(3, 3), 0);
+		cv::threshold(frame_bulb, frame_bulb, 220, 255, cv::THRESH_BINARY);
+		cv::dilate(frame_bulb, frame_bulb, m_kernelDilate_detectBulb);
+
+		cv::Canny(
+			frame_bulb,
+			frame_canny, 50, 150, 3);
+		cv::findContours(
+			frame_canny,
+			vecContours,
+			vecHierarchy,
+			cv::RETR_TREE,
+			cv::CHAIN_APPROX_SIMPLE,
+			cv::Point(0, 0));
+
+		vecMoments.reserve(vecContours.size());
+		vecCentralMoments.reserve(vecContours.size());
+
+		for (int i = 0; i < vecContours.size(); i++){
+			vecMoments[i] =
+				cv::moments(vecContours[i], false); 		//moments for each blob / contour
+			vecCentralMoments[i] =
 				cv::Point2f(											//center of each blob / contour
-					m_vecMoments_detectLight[i].m10 / m_vecMoments_detectLight[i].m00, 
-					m_vecMoments_detectLight[i].m01 / m_vecMoments_detectLight[i].m00); 		
+					vecMoments[i].m10 / vecMoments[i].m00,
+					vecMoments[i].m01 / vecMoments[i].m00);
 		}
 
-		cv::Mat drawing(m_imgCanny_detectLight.size(), CV_8UC3, cv::Scalar(255, 255, 255));
-		cv::Scalar color = cv::Scalar(167, 151, 0);
+		double area1 = 0.0;
+		double area2 = 0.0;
+		cv::Point2d retPoint;
 
-		for (int i = 0; i < m_vecContours_detectLight.size(); i++)
+		if (vecContours.size() > 1){
+			area1 = cv::contourArea(vecContours[0]);
+			retPoint = vecCentralMoments[0];
+
+			for (int i = 1; i < vecContours.size(); i++){
+				area2 = cv::contourArea(vecContours[i]);
+				if (area2 > area1){
+					retPoint = vecCentralMoments[i];
+					area1 = area2;
+				}
+			}
+		}
+		return retPoint;
+	}
+
+	bool LightTracker::bulbVsMask(const std::vector<cv::Point>& bulbContour, const cv::Mat& mask)
+	{
+		for (const auto& bC : bulbContour)
 		{
-			cv::drawContours(drawing, m_vecContours_detectLight, i, color, 2, 8, m_vecHierarchy_detectLight, 0, cv::Point());
-			cv::circle(drawing, m_vecCentralMoments_detectLight[i], 4, -1, 0);
+			if (mask.at<uint8_t>(bC) == 255) 
+				return true;
 		}
+		return false;
+	}
 
-		cv::Point2f retPoint;
 
-		if (m_vecContours_detectLight.size() == 1)
+	void LightTracker::imshow(
+		bool previousFrame,
+		bool currentFrame, 
+		bool lightThresh1, 
+		bool lightThresh2, 
+		bool lightMask, 
+		bool bulb, 
+		bool contoursMoments)
+	{
+		if (previousFrame) cv::imshow(m_sPreviousFrame, m_imgPreviousFrame);
+		else cv::destroyWindow(m_sPreviousFrame);
+		if (currentFrame)	cv::imshow(m_sCurrentFrame, m_imgCurrentFrame);
+		else cv::destroyWindow(m_sCurrentFrame);
+		if (lightThresh1)	cv::imshow(m_sLightThresh1, m_imgLightThresh1);
+		else cv::destroyWindow(m_sLightThresh1);
+		if (lightThresh2)	cv::imshow(m_sLightThresh2, m_imgLightThresh2);
+		else cv::destroyWindow(m_sLightThresh2);
+		if (lightMask) 		cv::imshow(m_sLightMask, m_imgLightMask);
+		else cv::destroyWindow(m_sLightMask);
+		if (bulb)			cv::imshow(m_sBulb, m_imgBulb_detectBulb);
+		else cv::destroyWindow(m_sBulb);
+		if (contoursMoments)
 		{
+			m_imgContoursMoments = cv::Mat(m_imgCanny_detectBulb.size(), CV_8UC3, cv::Scalar(255, 255, 255));
+			cv::Scalar color = cv::Scalar(167, 151, 0);
 
+			for (int i = 0; i < m_vecContours_detectBulb.size(); i++)
+			{
+				cv::drawContours(m_imgContoursMoments, m_vecContours_detectBulb, i, color, 2, 8, m_vecHierarchy_detectBulb, 0, cv::Point());
+				cv::circle(m_imgContoursMoments, m_vecCentralMoments_detectBulb[i], 4, -1, 0);
+			}
+			cv::imshow(m_sContoursMoments, m_imgContoursMoments);
 		}
-
-		cv::imshow("drawing", drawing);
-
-
-
-
-
-
-		//return cv::Point2f(m.m10 / m.m00, m.m01 / m.m00);
-		return cv::Point(0, 0);
+		else cv::destroyWindow(m_sContoursMoments);
 	}
 	
+	
+
+	Bulb::Bulb(cv::Point2d& pos, int32_t frameNumber, bool visible)
+		: m_position(pos), m_frameNumber(frameNumber), m_visible(visible), m_velocity(0.0), m_direction(cv::Vec2d(0,0))
+	{
+	}
+	//set motion relative to bulb in previous frame
+	void Bulb::setMotion(const bs::Bulb* b)
+	{
+		m_direction = m_position - b->m_position;
+		m_velocity = cv::sqrt(cv::pow(m_direction[0], 2) + cv::pow(m_direction[1], 2));
+	}
+
 }
--- MultiCameraLightTracking/LightTracker.h
+++ MultiCameraLightTracking/LightTracker.h
@@ -5,33 +5,44 @@
 
 namespace bs
 {
-	struct Blob
+	struct Bulb
 	{
-		cv::Point2f m_position;
-		cv::Vec2f m_direction;
-		float m_area;
-		float m_velocity;
+		cv::Point2d m_position;
+		int32_t m_frameNumber;
+		bool m_visible;
 
-		Blob(cv::Point2f pos);
+		cv::Vec2d m_direction;
+		double m_velocity;
+
+
+		Bulb(cv::Point2d& pos, int32_t frameNumber, bool visible = true);
+		void setMotion(const bs::Bulb* b);
+	
 	};
 
 	class LightTracker
 	{
 	private:
-
+		std::vector<bs::Bulb> m_vecBulbs;
+		
 		//window names
-		std::string m_sCurrentFrame	{ "current frame" };
-		std::string m_sLightThresh1	{ "light thresh 1" };
-		std::string m_sLightThresh2	{ "light htresh 2" };
-		std::string m_sLightMask	{ "light mask" };
-		std::string m_sBulb			{ "bulb" };
+		std::string m_sPreviousFrame	{ "previous frame" };
+		std::string m_sCurrentFrame		{ "current frame" };
+		std::string m_sLightThresh1		{ "light thresh 1" };
+		std::string m_sLightThresh2		{ "light htresh 2" };
+		std::string m_sLightMask		{ "light mask" };
+		std::string m_sBulb				{ "bulb" };
+		std::string m_sContoursMoments	{ "contours & central moment" };
+
 
 		//frames
+		cv::Mat m_imgFirstBulbFrame;
 		cv::Mat m_imgPreviousFrame;
 		cv::Mat m_imgCurrentFrame;
 		cv::Mat m_imgLightThresh1;
 		cv::Mat m_imgLightThresh2;
 		cv::Mat m_imgLightMask;
+		cv::Mat m_imgContoursMoments;
 
 		bs::VideoCaptureYUV* m_video;
 		uint32_t m_frameCounter{ 0 };
@@ -40,19 +51,19 @@ namespace bs
 
 		//fun thresholdLights
 		cv::Mat m_kernelDilate_thresholdLights;
-		int32_t m_thresh_thresholdLights;
+		int32_t m_thresh_thresholdLights{ 210 };
 
 		//fun detectLight
-		cv::Mat m_imgThresh_detectLight;
-		cv::Mat m_imgBulb_detectLight;
-		cv::Mat m_kernelDilate_detectLight;
-		cv::Mat m_kernelErode_detectLight;
+		cv::Mat m_imgThresh_detectBulb;
+		cv::Mat m_imgBulb_detectBulb;
+		cv::Mat m_kernelDilate_detectBulb;
+		cv::Mat m_kernelErode_detectBulb;
 
-		cv::Mat								m_imgCanny_detectLight;
-		std::vector<std::vector<cv::Point>> m_vecContours_detectLight;
-		std::vector<cv::Vec4i>				m_vecHierarchy_detectLight;
-		std::vector<cv::Moments>			m_vecMoments_detectLight;
-		std::vector<cv::Point2f>			m_vecCentralMoments_detectLight;
+		cv::Mat								m_imgCanny_detectBulb;
+		std::vector<std::vector<cv::Point>> m_vecContours_detectBulb;
+		std::vector<cv::Vec4i>				m_vecHierarchy_detectBulb;
+		std::vector<cv::Moments>			m_vecMoments_detectBulb;
+		std::vector<cv::Point2d>			m_vecCentralMoments_detectBulb;
 
 
 	public:
@@ -60,10 +71,24 @@ namespace bs
 		LightTracker(bs::VideoCaptureYUV* video);
 		int32_t start();
 		~LightTracker();
+
 	protected:
 		void thresholdLights(const cv::Mat& frame, cv::Mat& imgThresh);
 		void createLightMask(const cv::Mat& frame1, const cv::Mat& frame2, cv::Mat& mask);
-		cv::Point2f detectLight(const cv::Mat& frame, const cv::Mat& mask);
+		cv::Point2d detectBulb(const cv::Mat& frame, const cv::Mat& mask);
+		cv::Point2d detectBulbInFirstFrame(const cv::Mat& frame, const cv::Mat& mask);
+		bool objectVsMask(const cv::Mat& frame, const cv::Mat& mask);
+		bool bulbVsMask(const std::vector<cv::Point>& bulbContour, const cv::Mat& mask);
+		cv::Point2d predictBulbPos(bs::Bulb& b);
+		void imshow(
+			bool previousFrame,
+			bool currentFrame, 
+			bool lightThresh1, 
+			bool lightThresh2, 
+			bool lightMask, 
+			bool bulb, 
+			bool contoursMoments);
+
 
 	};
 
@@ -71,6 +96,15 @@ namespace bs
 	enum STATE
 	{
 		END_OF_FILE
+
+	};
+
+	enum BULB_STATE
+	{
+		BULB_NOT_VISIBLE,
+		BULB_TOO_FAR,
+		BULB_OUT_OF_FRAME,
+		BULB_OVERLAPS_MASK,
 	};
 
 }
